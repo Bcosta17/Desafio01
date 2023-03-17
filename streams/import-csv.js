600d0e5eed02c7fb0624387d86a3b7c195f41fb9
Bruno Costa <bcosta17.bc@gmail.com>
2023-03-17 18:21:27 -0300

    streams

---
+++ streams/import-csv.js
@@ -0,0 +1,43 @@
+import { parse } from 'csv-parse';
+import fs from 'node:fs';
+import { Duplex } from 'node:stream';
+
+const csvPath = new URL('./tasks.csv', import.meta.url);
+
+const stream = fs.createReadStream(csvPath);
+
+const csvParse = parse({
+  delimiter: ',',
+  skipEmptyLines: true,
+  fromLine: 2 // skip the header line
+});
+
+async function run() {
+  const linesParse = stream.pipe(csvParse);
+
+  for await (const line of linesParse) {
+    const [title, description] = line;
+    
+    console.log(title)
+    
+    await fetch('http://localhost:3000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+      duplex: 'half'
+    })
+
+  }
+
+}
+
+run()
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
